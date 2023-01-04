27b244c81f460bbd004e478ad343ae9f58afb69a
nhutb1910275 <nhutb1910275@student.ctu.edu.vn>
2023-01-04 22:34:01 +0700

    Dinh nghia cac route cho tai nguyen contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req,res) =>{
+    res.send({message:"create hanlder"});
+};
+
+exports.findAll = (req,res) =>{
+    res.send({message:"findAll hanlder"});
+};
+
+exports.findOne = (req,res) =>{
+    res.send({message:"findOne hanlder"});
+};
+
+exports.update = (req,res) =>{
+    res.send({message:"update hanlder"});
+};
+
+exports.delete = (req,res) =>{
+    res.send({message:"delete hanlder"});
+};
+
+exports.deleteAll = (req,res) =>{
+    res.send({message:"update hanlder"});
+};
+
+exports.findAllFavorite = (req,res) =>{
+    res.send({message:"findAllFavorite hanlder"});
+};
