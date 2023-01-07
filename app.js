de3eb2e7925173c0c8e1540f49a67f2764a255df
nhutb1910275 <nhutb1910275@student.ctu.edu.vn>
2023-01-07 09:31:36 +0700

    cai dat xu li loi

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const cros = require("cors");
 const app = express();
+const ApiError = require("./app/app-error")
 app.use(cros());
 app.use(express.json())
 app.get("/", (req, res) => {
-res.json({message:"Welcome contact book application."});
+    res.json({ message: "Welcome contact book application." });
 });
 
 const contactsRouter = require("./app/routes/contact.route.js");
 
-app.use("/api/contacts",contactsRouter)
+app.use("/api/contacts", contactsRouter);
 
+//  cai dat cac xu li loi
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found !!!"));
+});
+
+app.use((err, req, res, next) => {
+
+    return res.status(Error.statusCode || 500).json({ message: Error.message || "Internal Server Error", });
+});
 
 module.exports = app;
--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -19,7 +19,7 @@ exports.delete = (req,res) =>{
 };
 
 exports.deleteAll = (req,res) =>{
-    res.send({message:"update hanlder"});
+    res.send({message:"deleteAll hanlder"});
 };
 
 exports.findAllFavorite = (req,res) =>{
